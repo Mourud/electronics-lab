d8bbb888f9d928a95008eed592f9723959becc81
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2023-02-18 22:17:40 -0800

    added ability to play baba black sheep on MSP430

---
+++ part7_beyond/main.c
@@ -0,0 +1,35 @@
+#include <msp430f5529.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    WDTCTL = WDTPW + WDTHOLD;
+    P1DIR |= BIT2; // Output on Pin 1.2
+    P1SEL |= BIT2; // Pin 1.2 selected as PWM
+
+    // microseconds
+    TA0CCTL1 = OUTMOD_7; // TA0CCR1 reset/set-high voltage
+    // below count, low voltage when past
+
+    TA0CTL = TASSEL_2 + MC_1 + TAIE + ID_0;
+    // Timer A control set to SMCLK, 1MHz
+    // and count up mode MC_1
+
+    // Make a list of 100 notes in Hz
+	// make a list of 200 random numbers
+    int notes[] = {261, 261, 392, 392, 440, 440, 392, 349, 349, 330, 330, 294, 294, 261, 392, 392, 349, 349, 330, 330, 294, 392, 392, 349, 349, 330, 330, 294};
+
+    while (1)
+    {
+
+    int i = 0;
+    for (i = 0; i < 28; i++){
+        int period = 1000000 / notes[i];
+        TA0CCR0 = period;
+        TA0CCR1 = period / 2;
+        __delay_cycles(500000);
+
+    }
+    }
+}
